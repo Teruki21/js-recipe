85eef20a5b1d986ba6f7d51f889bd82ebad5247f
Teruki21 <sano.teruki.r2@dc.tohoku.ac.jp>
2022-10-08 14:00:04 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -9,3 +9,41 @@ const genkiFunction = function (number) {
     }
   }
 }
+
+// 発展1 1 ~ 引数までのFizzBuzzを表示する関数 FizzBuzz
+const FizzBuzz = function (number) {
+  for (let m = 1; m <= number; ++m) {
+    if (m % 3 === 0 && m % 5 == 0) {
+      console.log("FizzBuzz")
+    } else if (m % 3 === 0) {
+      console.log("Fizz")
+    } else if (m % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(m)
+    }
+  }
+}
+
+// 発展2 3が含む時のプログラム
+const genkiFunction3 = function (number) {
+  for (let n = 1; n <= number; ++n) {
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      let k = n
+      // 3を含むか判別
+      while (k > 0) {
+        if (k % 10 === 3) {
+          console.log(`${n}!!!!!!!`)
+          break
+        } else {
+          k /= 10
+        }
+      }
+      if (k === 0) {
+        console.log(n)
+      }
+    }
+  }
+}
