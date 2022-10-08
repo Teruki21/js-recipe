6a5962a4e8906bcb529d24aef950b1aba232449c
Teruki21 <sano.teruki.r2@dc.tohoku.ac.jp>
2022-10-08 12:59:47 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -1,14 +1,18 @@
 // 練習問題 １
-const plus1 = function (x) {
-  return x + 1
-}
+//const plus1 = function (x) {
+//  return x + 1
+
 // 関数名:
 // 引数:
 // 返り値:
 
 // 練習問題 ２
 // f1: 1, 4, 7, 10, 13, 16, ...
+const f1 = function (n) {
+  return 3 * n + 1
+}
 
+console.log(f1(6))
 // f2: 1, 10, 100, 1000, 10000, ...
 
 // f3: 2, 14, 107, 1010, 10013, ...
